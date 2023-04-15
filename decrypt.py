c7f679f709327848bacc2edccbe81eacb9aabc31
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-15 14:45:00 +0800

    Finally importing termcolor worked, this will help color even the pyfiglet fonts

--- decrypt.py
+++ decrypt.py
@@ -1,8 +1,22 @@
 # Laguting, Maricon Jane G. 
 # BSCpE 1-4
-# Task: asDecrypt an encrypted text entered by the user
+# Task: Decrypt an encrypted text entered by the user
 # Welcome the user
+from pyfiglet import Figlet
+from termcolor import colored
+welcome_usr = Figlet(font = "banner3-D")
+print("=" * 94)
+print(colored(welcome_usr.renderText(" Welcome!"), "yellow"))
+print("=" * 94)
+
 # Ask an input from the user
 # Process the input
 # a, e, i, o, u assigned to *, &, #, +, ! respectively using Tuple
-# 
+# Checking
+  # If *, change to "a"
+  # If &, change to "e"
+  # If #, change to "i"
+  # If +, change to "o"
+  # If !, change to "u"
+# Print the decrypted input
+# Program closing
