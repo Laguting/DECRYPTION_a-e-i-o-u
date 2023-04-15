1843ca69763ff0cafccdeb23a5401afb72a926b5
Laguting <lagutingmariconjaneg@gmail.com>
2023-04-15 13:07:30 +0800

    Writting down the pseudocode

---
+++ decrypt.py
@@ -0,0 +1,8 @@
+# Laguting, Maricon Jane G. 
+# BSCpE 1-4
+# Task: asDecrypt an encrypted text entered by the user
+# Welcome the user
+# Ask an input from the user
+# Process the input
+# a, e, i, o, u assigned to *, &, #, +, ! respectively using Tuple
+# 
